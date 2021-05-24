8f41a604f3c9e47197e79b17b26997e598278045
Pavllov <glebotvet@yandex.ru>
2021-05-25 00:47:57 +0300

    Зачёт

---
+++ Зачётная работа/Zachet/Properties/AddStudents.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zachet.Properties
+{
+	/// <summary>
+	/// Логика взаимодействия для AddStudents.xaml
+	/// </summary>
+	public partial class AddStudents : Page
+	{
+		public AddStudents()
+		{
+			InitializeComponent();
+			CBSPECI.SelectedValuePath = "Id";
+			CBSPECI.DisplayMemberPath = "Name";
+			CBSPECI.ItemsSource = BD.bazaDannix.Curs.ToList();
+			CBFormTeach.SelectedValuePath = "Id";
+			CBFormTeach.DisplayMemberPath = "Name";
+			CBFormTeach.ItemsSource = BD.bazaDannix.FormaTeaching.ToList();
+			CBNameGroup.SelectedValuePath = "Id";
+			CBNameGroup.DisplayMemberPath = "Name";
+			CBNameGroup.ItemsSource = BD.bazaDannix.Group.ToList();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				FIO fIO = new FIO()
+				{
+					FullName = TBFIO.Text,
+					IdGroup = (int)CBNameGroup.SelectedValue,
+					IdFormaTeaching = (int)CBFormTeach.SelectedValue,
+					GodPostuplenia = Convert.ToInt32 (TbData.Text),
+					IdCurs = (int)CBSPECI.SelectedValue,
+				};
+				BD.bazaDannix.FIO.Add(fIO);
+				BD.bazaDannix.SaveChanges();
+				MessageBox.Show("Студент добавлен","Поздравляю!",MessageBoxButton.OK);
+
+			}
+			catch (Exception )
+			{
+
+				throw;
+			}
+
+		}
+	}
+}
---
+++ Зачётная работа/Zachet/Properties/Mark.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zachet.Properties
+{
+	/// <summary>
+	/// Логика взаимодействия для Mark.xaml
+	/// </summary>
+	public partial class Mark : Page
+	{
+		public Mark()
+		{
+			InitializeComponent();
+			CBGroup.SelectedValuePath = "Id";
+			CBGroup.DisplayMemberPath = "Name";
+			CBGroup.ItemsSource = BD.bazaDannix.Group.ToList();
+
+			CBStudent.SelectedValuePath = "Id";
+			CBStudent.DisplayMemberPath = "FullName";
+			CBStudent.ItemsSource = BD.bazaDannix.FIO.ToList();
+
+			CBDis.SelectedValuePath = "Id";
+			CBDis.DisplayMemberPath = "Name";
+			CBDis.ItemsSource = BD.bazaDannix.Disceplina.ToList();
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Jurnal jurnal = new Jurnal()
+				{
+					IdFIO = (int)CBStudent.SelectedValue,
+					IdDisceplina = (int)CBDis.SelectedValue,
+					Mark = Convert.ToInt32(TbMark.Text),
+
+				};
+				BD.bazaDannix.Jurnal.Add(jurnal);
+				BD.bazaDannix.SaveChanges();
+				MessageBox.Show("Оценка студенту поставлена","Поздравляю!",MessageBoxButton.OK);
+
+			}
+			catch (Exception )
+			{
+
+				throw;
+			}
+		}
+	}
+}
